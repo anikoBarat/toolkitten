c3debfcf2b2af43af1a1ce8a0e1f4d4ed6afe0e9
anikoBarat <aniko.barat@gmail.com>
2018-08-17 14:14:30 +0200

    5th week 1st day codes

---
+++ summer-of-code/week-05/day1_homework.js
@@ -0,0 +1,12 @@
+/// 1. Hours in a year. How many hours are in a year?
+console.log("Hours in a year: ", (24*365))
+
+/// 2. Minutes in a decade. How many minutes are in a decade?
+console.log("Minutes in a decade: ", (60*24*365*10))
+
+/// 3. Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+console.log("My age in seconds: ", (60*60*24*365*30))
+
+/// 4. Cristina Tarantino: 32 yesterday! How many milliseconds old is she hahaha? 
+/// Calculate @Cristina Tarantino's age in milliseconds.
+console.log("Cristina Tarantino's age in milliseconds: ", (1000*60*60*24*365*30))
