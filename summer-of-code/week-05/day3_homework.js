74fa9b1c0299ce75bcb310210db6bee159bec13d
anikoBarat <aniko.barat@gmail.com>
2018-08-18 18:36:12 +0200

    Bigger, better num & angry boss task is ready with basic html implementation

--- summer-of-code/week-05/day3_homework.js
+++ summer-of-code/week-05/day3_homework.js
@@ -1,7 +1,7 @@
 /*  ### homeWork ####
  
  1. Full name greeting. Write a program that asks for a person’s first name, then middle, 
-    and then last. Finally, it should greet the person using their full name. 
+    and then last. Finally, it should greet the person using their full name. name
 
  2. Bigger, better favorite number. Write a program that asks for a person’s favorite number. 
     Have your program add 1 to the number, and then suggest the result as 
@@ -70,3 +70,24 @@ startButton.addEventListener("click", function () {
 */
 
 // 2. 
+
+function biggerNum(){
+    alert("event ok");
+    var userNum = inputField.value;
+    inputField.value = "";
+    alert("I suggest you a bigger and better number: " + (parseInt(userNum) + 1));
+}
+
+var inputField = document.getElementById("input");
+inputField.addEventListener("change", biggerNum);
+
+
+// 3.
+
+function angryBoss(){
+    var ask = document.getElementById("angry_input").value;
+    alert("WADDAYA MEAN ON '" + ask + "'?!\nYou are FIRED!!!!");
+    
+}
+
+document.getElementById("angry_input").addEventListener("change", angryBoss);
